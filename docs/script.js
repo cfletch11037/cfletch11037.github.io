6c02d1635e5d348119c512e873eb1ba0e08e8767
cfletch11037 <cfletcher0901@gmail.com>
2026-09-03 08:11:55 -0400

    Count button clicks

--- docs/script.js
+++ docs/script.js
@@ -1,2 +1,9 @@
-let myString = "a "
-alert(myString.repeat(3));
+let count = 0
+
+const button = document.getElementById("countButton");
+const display = document.getElementById("numClicks");
+
+button.addEventListener("click", () => {
+    count++;
+    display.textContent = count;
+});
