ad8a75431996b129a714eb6a05a6cf726686deb8
cfletch <cfletch11037@gmail.com>
2026-09-20 17:35:33 -0400

    testing 2: electric boogaloo

--- docs/slider.js
+++ docs/slider.js
@@ -1,7 +1,7 @@
 const slider1 = document.getElementById("love");
 const display1 = document.getElementById("ratingDisplay");
 
-slider.addEventListener("input", () => {
+slider1.addEventListener("input", () => {
     display1.textContent = slider1.value;
     alert(slider1.value);
 });
